a7e81f7d8dc0ac4931529befc67697226ba064fb
Mark Glasgow <markglasgow@gmail.com>
2019-01-15 16:14:47 +0000

    5

--- rango/urls.py
+++ rango/urls.py
@@ -9,6 +9,6 @@ from rango import views
 
 urlpatterns = [ 
     url(r'^$', views.index, name='index'),
-#    url(r'^about/', views.about, name='about'), 
+    url(r'^about/', views.about, name='about'), 
  
     ]
--- rango/views.py
+++ rango/views.py
@@ -4,3 +4,7 @@ def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
 
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+
+    return render(request, 'rango/about.html')
