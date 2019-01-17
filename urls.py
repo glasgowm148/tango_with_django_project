e5d05694d72924588cd597e7f3cf2f70f402afc4
Mark Glasgow <markglasgow@gmail.com>
2019-01-17 16:34:57 +0000

    ch4 begin

--- urls.py
+++ urls.py
@@ -4,8 +4,7 @@ from django.conf.urls import include
 from rango import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'), url(r'^rango/', include('rango.urls')), # above maps any URLs starting
-    # with rango/ to be handled by
-    # the rango application
+    url(r'^$', views.index, name='index'), 
+    url(r'^rango/', include('rango.urls')), 
     url(r'^admin/', admin.site.urls),
     ]
