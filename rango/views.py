2ae1db33b5d6f065f6b572186ab8e609185e0433
Mark Glasgow <markglasgow@gmail.com>
2019-01-17 21:40:36 +0000

    1fail/1error ch6 tests

--- rango/views.py
+++ rango/views.py
@@ -27,4 +27,4 @@ def show_category(request, category_name_slug):
     except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
-    return render(request, 'range/category.html', context_dict)
+    return render(request, 'rango/category.html', context_dict)
