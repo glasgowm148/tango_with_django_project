b0a7a77b02c626f1f6d9390b8f8eeab64c762213
Mark Glasgow <markglasgow@gmail.com>
2019-01-21 16:15:52 +0000

    9.6 start

--- rango/admin.py
+++ rango/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 
 
@@ -16,6 +16,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
 
 
 
